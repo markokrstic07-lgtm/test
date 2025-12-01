3f4f95dd7f7f9702790a5aae536f84bca0980d2f
markokrstic07-lgtm <markokrstic07@gmail.com>
2025-12-01 12:21:22 +0100

    Štampanje 36x Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
         for (int i = 0; i < 36; i++) {
-            System.out.println("Hello, World");
+            System.out.println("Hello World");
         }
     }
 }
